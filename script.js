cf6cd4b8576e7d8b503a363cb6d62176cc58ccc3
Asif Iqbal <30491169+asfi-commit@users.noreply.github.com>
2022-06-17 16:26:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Script Running....")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+})
